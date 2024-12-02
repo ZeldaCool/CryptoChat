36cb60c0f6483a2bb95acce7cc16775d6bf1dd18
Person Boy <atticusfinch570@gmail.com>
2024-12-01 16:16:08 -0800

    aaa

--- Client1/SocketSetupClient1/SocketConnecterClient1.py
+++ Client1/SocketSetupClient1/SocketConnecterClient1.py
@@ -5,19 +5,19 @@ from ssl import SSLContext
 import certifi
 import socket
 import ssl
-import SocketConnecterClient2
+import serversocket
 
 hostname = socket.gethostname()
 PORT = 400
 host = socket.gethostbyname(hostname)
 
-data = input('Message:')
-data = host + ": " + data
+ata = input('Message:')
+ata = host + ": " + ata
 def sender():
     erver = (serversocket.HOST, 4000)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host,PORT))
-    s.sendto(data.encode('utf-8'),erver)
+    s.sendto(ata.encode('utf-8'),erver)
     print(s.recv(1024))
     s.close()
 sender()
--- Client2/SocketSetupClient2/SocketConnecterClient2.py
+++ Client2/SocketSetupClient2/SocketConnecterClient2.py
@@ -4,16 +4,20 @@ import certifi
 import socket
 import ssl
 
+data = input('INPUT:')
 hostname = socket.gethostname()
 HOST = socket.gethostbyname(hostname)
 PORT = 4000
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST,PORT))
-data,addr= s.recvfrom(1024)
-data = data.decode('utf-8')
-print("Message from: " + str(addr))
-print(data)
-data = input('INPUT:')
-data = data.encode('utf-8')
-print("Sending: " + data)
-s.sendto(data.encode('utf-8'), addr)
+firstTime = True
+def mainloopy ():
+    while data != 'quit':
+        if not firstTime == True:
+            print("Sending: " + data)
+            data = data.encode('utf-8')
+            s.sendto(data.encode('utf-8'), addr)
+        elif not firstTime :
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.bind((HOST, PORT))
+            data, addr = s.recvfrom(1024)
+            data = data.decode('utf-8')
+            print("Message from: " + str(addr))
